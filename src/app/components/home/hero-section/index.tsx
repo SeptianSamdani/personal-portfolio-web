08c5c9a4d40027272fb773bb76d1232e2b3f25ad
SeptianSamdany <227007072@student.unsil.ac.id>
2026-04-01 13:22:01 +0700

    update profile hero section margin

--- src/app/components/home/hero-section/index.tsx
+++ src/app/components/home/hero-section/index.tsx
@@ -66,7 +66,7 @@ const HeroSection = () => {
 
                     {/* Avatar */}
                     <div className="relative w-full lg:w-1/2 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-none mx-auto">
-                        <div className="relative w-full h-[300px] sm:h-[400px] lg:h-[520px] xl:h-[620px] ml-6">
+                        <div className="relative w-full h-[300px] sm:h-[400px] lg:h-[520px] xl:h-[620px]">
                             <Image
                                 src={"/images/home/banner/profile.png"}
                                 alt="Septian Samdani"
